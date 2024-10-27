2ddd9ba768c4a6f6a94627b042ab725e571f5a8e
siva <siva@gmail.com>
2024-10-27 20:16:20 +0530

    Added first code

---
+++ vars/dockerPipeline.groovy
@@ -0,0 +1,29 @@
+import com.i27academy.builds.Calculator
+
+def call(Map pipelineparams){
+    // An instance of the class called calculator is created
+    Calculator calculator = new Calculator(this)
+
+    pipeline {
+        agent any 
+        environment {
+            APP_NAME = "${pipelineparams.appName}" // this value should be coming from microservices 
+        }
+        stages {
+            stage('AdditionStage') {
+                steps {
+                    script {
+                    echo "Printing Sum of 2 numbers"
+                    println calculator.add(3,4) // add expects 2 parameters
+                    echo "****** Microservice Name is: ${APP_NAME} ********"
+                    }
+                }
+            }
+            stage ('SecondStage') {
+                steps {
+                    echo "Printing second stage"
+                }
+            }
+        }
+    }
+}
