8dce18fc58bcac0e8b010e7e9e2d433071bfc193
siva <siva@gmail.com>
2024-10-27 20:30:16 +0530

    added build src code

---
+++ src/com/i27academy/builds/Docker.groovy
@@ -0,0 +1,28 @@
+package com.i27academy.builds
+
+// all the methods 
+class Calculator {
+    def jenkins
+    Docker(jenkins) {
+        this.jenkins = jenkins
+    }
+
+
+    // Application Build
+    def buildApp(appName){
+        jenkins.sh """
+            echo "Building the $appName Application"
+            mvn clean package -DskipTests=true
+        """
+    }
+
+
+}
+
+
+def buildApp() {
+    return {
+        echo "Building the ${env.APPLICATION_NAME} Application"
+        sh 'mvn clean package -DskipTests=true'
+    }
+}
