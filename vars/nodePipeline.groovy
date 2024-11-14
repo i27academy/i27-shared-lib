b8f3fb27a466e454d5b6614bdda3ef81d067be51
siva <siva@gmail.com>
2024-11-14 15:14:34 +0530

    implemented node pipeline

--- vars/nodePipeline.groovy
+++ vars/nodePipeline.groovy
@@ -1,11 +1,92 @@
-// Create a pipeline for frontend application deployment 
+import com.i27academy.builds.Calculator
+import com.i27academy.builds.Docker
+import com.i27academy.k8s.K8s
 
-build the docker image
-deploying to dev env 
+def call(Map pipelineParams){
+    // An instance of the class called calculator is created
+    Calculator calculator = new Calculator(this)
+    Docker docker = new Docker(this)   
+    K8s k8s = new K8s(this) 
 
-deploying to tst env 
-    test
-deploying to stage env
-    stage
-deploying to prod env 
-    prod
+// This Jenkinsfile is for Eureka Deployment 
+
+    pipeline {
+        agent {
+            label 'k8s-slave'
+        }
+        parameters {
+            choice(name: 'dockerPush',
+                choices: 'no\nyes',
+                description: 'This Will build dockerImage and Push'
+            )
+        }
+
+        environment {
+            APPLICATION_NAME = "${pipelineParams.appName}"
+            DOCKER_HUB = "docker.io/i27devopsb4"
+            DOCKER_CREDS = credentials('dockerhub_creds') //username and password
+        }
+        stages {
+            stage ('Docker Build and Push') {
+                when {
+                    anyOf {
+                        expression {
+                            params.dockerPush == 'yes'
+                        }
+                    }
+                }
+                steps { 
+                    script {
+                        dockerBuildAndPush().call()
+                    }
+                } 
+            }
+        }
+    }
+}
+
+
+// Method for Docker build and Push
+def dockerBuildAndPush(){
+    return {
+        echo "************************* Building Docker image*************************"
+        sh "docker build --no-cache -t ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT} ./.cicd"
+        echo "************************ Login to Docker Registry ************************"
+        sh "docker login -u ${DOCKER_CREDS_USR} -p ${DOCKER_CREDS_PSW}"
+        sh "docker push ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
+    }
+}
+
+def imageValidation() {
+    return {
+        println("Attemting to Pull the Docker Image")
+        try {
+            sh "docker pull ${env.DOCKER_HUB}/${env.APPLICATION_NAME}:${GIT_COMMIT}"
+            println("Image is Pulled Succesfully!!!!")
+        }
+        catch(Exception e) {
+            println("OOPS!, the docker image with this tag is not available,So Creating the Image")
+            buildApp().call()
+            dockerBuildAndPush().call()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
